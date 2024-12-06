dd3bd343fb80bb9a807c1067c8acb52fb5e3c7ef
perni24 <perni24lb@gmail.com>
2024-12-06 18:08:34 +0100

    modifiche 06/12/2024

--- src/components/gestionePresenze/gestionePresenze.jsx
+++ src/components/gestionePresenze/gestionePresenze.jsx
@@ -18,21 +18,32 @@ function GestionePresenze() {
   return (
     <>
     <div id="containerApp">
+      <select name="classe" id="classe" onChange={(e) => setClasse(parseInt(e.target.value, 10))}>
+        <option value="1">prima elemntare</option>
+        <option value="2">seconda elemntare</option>
+        <option value="3">terza elemntare</option>
+        <option value="4">quarta elemntare</option>
+        <option value="5">quinta elemntare</option>
+      </select>
       <table>
+        <thead>
         <tr>
             <th>Alunno</th>
             <th>Presenza</th>
         </tr>
+        </thead>
+        <tbody>
         {alunni
           .filter((item) => item.classe === classe) //filtro sulla classe
           .map((item, index) => (
-            <tr>
+            <tr key={index}>
                 <td key={index}>
                 {item.nome} {item.cognome}
                 </td>
-                <td><input type="checkbox"/></td>
+                <td className="checkbox"><input type="checkbox"/></td>
             </tr>
           ))}
+        </tbody>
       </table>
     </div>
     </>
