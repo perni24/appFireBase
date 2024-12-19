b2580452735e26c16c438f975b0791ad9a426c78
perni24 <perni24lb@gmail.com>
2024-12-19 23:46:25 +0100

    modifiche 19/12/2024

--- src/components/gestionePresenze/gestionePresenze.jsx
+++ src/components/gestionePresenze/gestionePresenze.jsx
@@ -8,13 +8,30 @@ function GestionePresenze() {
   useEffect(() => {
     // Simula il recupero dei dati degli alunni
     let dati = [
-      { nome: "Luca", cognome: "Bellini", classe: 1, presenza: false},
-      { nome: "Mirco", cognome: "Bellini", classe: 1, presenza: true},
-      { nome: "Valentina", cognome: "Bellini", classe: 1, presenza: false},
+      { id : 1, nome: "Luca", cognome: "Bellini", classe: 1, presenza: false},
+      { id : 2, nome: "Mirco", cognome: "Bellini", classe: 1, presenza: true},
+      { id : 3, nome: "Valentina", cognome: "Bellini", classe: 1, presenza: false},
     ];
 
     setAlunni(dati); // Imposta i dati degli alunni nello stato
   }, []); // L'array vuoto come secondo argomento fa sì che l'effetto venga eseguito solo una volta al montaggio del componente
+
+  const changePresenza = (id, stato) => {
+    // Funzione per cambiare lo stato della presenza di un alunno
+    let ris = []
+
+    alunni.map(element => {
+      if(element.id === id ){
+        ris.push({ ...element, presenza: stato });
+      }
+      else{
+        ris.push(element);
+      }
+    });
+  
+    // Aggiorna lo stato della classe con il nuovo array
+    setAlunni(ris);
+  };
 
   return (
     <>
@@ -45,7 +62,7 @@ function GestionePresenze() {
                     {item.nome} {item.cognome}
                   </td>
                   <td className="checkbox">
-                    <input type="checkbox" checked={item.presenza} onClick={() => changePresenza()}/>
+                    <input type="checkbox" checked={item.presenza} onChange={(e) => changePresenza(item.id, e.target.checked)}/>
                   </td>
                 </tr>
               ))}
